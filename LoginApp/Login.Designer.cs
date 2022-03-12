2db00c3f5716d6da5f4871b89ec1cc7d586ad6f9
Tamoza <101449907+Tamoza4@users.noreply.github.com>
2022-03-12 11:41:28 +0300

    Add files via upload

---
+++ LoginApp/Login.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace LoginApp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.TextUserName = new System.Windows.Forms.TextBox();
+            this.TextPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TextLoginError = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkSaveLogin = new System.Windows.Forms.CheckBox();
+            this.btnregister = new LoginApp.TamozaButton();
+            this.btnExit = new LoginApp.TamozaButton();
+            this.btnLogin = new LoginApp.TamozaButton();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextUserName
+            // 
+            this.TextUserName.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold);
+            this.TextUserName.Location = new System.Drawing.Point(129, 167);
+            this.TextUserName.Name = "TextUserName";
+            this.TextUserName.Size = new System.Drawing.Size(350, 22);
+            this.TextUserName.TabIndex = 0;
+            // 
+            // TextPass
+            // 
+            this.TextPass.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold);
+            this.TextPass.Location = new System.Drawing.Point(129, 203);
+            this.TextPass.Name = "TextPass";
+            this.TextPass.Size = new System.Drawing.Size(350, 22);
+            this.TextPass.TabIndex = 1;
+            this.TextPass.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(48, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(48, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(203, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(169, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TextLoginError
+            // 
+            this.TextLoginError.AutoSize = true;
+            this.TextLoginError.Font = new System.Drawing.Font("Times New Roman", 10.75F);
+            this.TextLoginError.ForeColor = System.Drawing.Color.Red;
+            this.TextLoginError.Location = new System.Drawing.Point(120, 263);
+            this.TextLoginError.Name = "TextLoginError";
+            this.TextLoginError.Size = new System.Drawing.Size(0, 17);
+            this.TextLoginError.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(129, 292);
+            this.progressBar1.Maximum = 80;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(350, 23);
+            this.progressBar1.Step = 100;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 7;
+            this.progressBar1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 30;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkSaveLogin
+            // 
+            this.checkSaveLogin.AutoSize = true;
+            this.checkSaveLogin.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold);
+            this.checkSaveLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkSaveLogin.Location = new System.Drawing.Point(129, 238);
+            this.checkSaveLogin.Name = "checkSaveLogin";
+            this.checkSaveLogin.Size = new System.Drawing.Size(86, 19);
+            this.checkSaveLogin.TabIndex = 9;
+            this.checkSaveLogin.Text = "Save Login";
+            this.checkSaveLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnregister
+            // 
+            this.btnregister.BackColor = System.Drawing.Color.Blue;
+            this.btnregister.BackgroundColor = System.Drawing.Color.Blue;
+            this.btnregister.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnregister.BorderRadius = 20;
+            this.btnregister.BorderSize = 0;
+            this.btnregister.FlatAppearance.BorderSize = 0;
+            this.btnregister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnregister.ForeColor = System.Drawing.Color.White;
+            this.btnregister.Location = new System.Drawing.Point(222, 394);
+            this.btnregister.Name = "btnregister";
+            this.btnregister.Size = new System.Drawing.Size(150, 40);
+            this.btnregister.TabIndex = 10;
+            this.btnregister.Text = "Register";
+            this.btnregister.TextColor = System.Drawing.Color.White;
+            this.btnregister.UseVisualStyleBackColor = false;
+            this.btnregister.Click += new System.EventHandler(this.btnregister_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Red;
+            this.btnExit.BackgroundColor = System.Drawing.Color.Red;
+            this.btnExit.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnExit.BorderRadius = 20;
+            this.btnExit.BorderSize = 0;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(129, 338);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(150, 40);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.TextColor = System.Drawing.Color.White;
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Green;
+            this.btnLogin.BackgroundColor = System.Drawing.Color.Green;
+            this.btnLogin.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btnLogin.BorderRadius = 20;
+            this.btnLogin.BorderSize = 0;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(329, 338);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(150, 40);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.TextColor = System.Drawing.Color.White;
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 19.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(247, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 31);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Login";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnregister);
+            this.Controls.Add(this.checkSaveLogin);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.TextLoginError);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextPass);
+            this.Controls.Add(this.TextUserName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Login_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextUserName;
+        private System.Windows.Forms.TextBox TextPass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private TamozaButton btnLogin;
+        private TamozaButton btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label TextLoginError;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkSaveLogin;
+        private TamozaButton btnregister;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LoginApp/Login.cs
@@ -0,0 +1,170 @@
+﻿// Created By Tamoza
+// www.tamoza.net
+
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginApp
+{
+    public partial class Login : Form
+    {
+
+        // please put your database details in Login and register forms -- replace "****" to your details
+
+        MySqlConnection con = new MySqlConnection(@"
+        Data Source=****;
+        Initial Catalog=****; 
+        User Id=****;
+        password=****");
+        
+        int i;
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+        // Free move for The form
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                i = 0;
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Logindb where UserName= '" + TextUserName.Text + "' and password= '" + TextPass.Text + "'";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+                i = Convert.ToInt32(dt.Rows.Count.ToString());
+
+
+                if (i == 0)
+                {
+                    TextLoginError.Text = "User name or Password is invalid, Please contact the support";
+                }
+                else
+                {
+                    btnLogin.Enabled = false;
+                    TextLoginError.ForeColor = Color.Green;
+                    this.TextLoginError.Text = "                                   connecting.";
+                    progressBar1.Visible = true;
+                    this.timer1.Start();
+                    await Task.Delay(1000);
+                    this.TextLoginError.Text = "                                   connecting..";
+                    await Task.Delay(1000);
+                    this.TextLoginError.Text = "                                   connecting...";
+                    await Task.Delay(1000);
+                    this.TextLoginError.Text = "                                   connected";
+                    await Task.Delay(500);
+                    //await Task.Delay(3000);
+                    Welcome welc = new Welcome();
+                    welc.Show();
+                    Hide();
+                }
+
+                con.Close();
+
+            }
+            catch
+            {
+                TextLoginError.Text = "Error with Database";
+            }
+
+
+            if (checkSaveLogin.Checked)
+            {
+                // Saveing Login detiles as config.txt file in program folder
+                System.IO.File.WriteAllText("Config.txt",
+                                this.TextUserName.Text.ToString()
+                                + System.Environment.NewLine
+                                + this.TextPass.Text.ToString()
+                                + System.Environment.NewLine
+                                + this.checkSaveLogin.Checked.ToString());
+            }
+            else
+            {
+                System.IO.File.Delete("Config.txt");
+            }
+
+
+
+
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.progressBar1.Increment(1);
+            
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            this.AcceptButton = btnLogin; // Enter key triggering the Login button 
+
+            // Load the login detiles from config.txt file
+            try
+            {
+                var TextLine1 = File.ReadLines("config.txt").Skip(0).First();
+                TextUserName.Text = TextLine1;
+                var TextLine2 = File.ReadLines("config.txt").Skip(1).First();
+                TextPass.Text = TextLine2;
+                var TextLine3 = File.ReadLines("config.txt").Skip(2).First();
+                checkSaveLogin.Checked = bool.Parse(TextLine3);
+            }
+            catch
+            {
+
+            }
+
+
+        }
+
+        private void Login_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void tamozaButton1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnregister_Click(object sender, EventArgs e)
+        {
+            register regi = new register();
+            regi.Show();
+            Hide();
+        }
+    }
+}
